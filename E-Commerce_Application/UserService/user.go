04b099b662403380ba95da14ec579cf5c6f38377
Maheshwar Punyam Anand <122685368+Maheshwarpa@users.noreply.github.com>
2025-03-06 11:39:44 -0700

    updated code

--- E-Commerce_Application/UserService/user.go
+++ E-Commerce_Application/UserService/user.go
@@ -2,19 +2,28 @@ package UserService
 
 import (
 	"fmt"
-	"log"
+	lg "module/logger"
 	"strconv"
-
-	"github.com/hashicorp/vault/api"
+	//"module/Database"
 )
 
+var Sd UserDetails
+
 type UserDetails struct {
-	Cust_Id    int     `json:"cust_is"`
+	Cust_Id    int     `json:"cust_id"`
 	Cust_Name  string  `json:"cust_name"`
 	Cust_Email string  `json:"cust_email"`
 	Cust_PNum  string  `json:"cust_pnum"`
 	Cust_Bal   float64 `json:"cust_bal"`
+	UserName   string  `json:"username"`
 }
+
+/*
+{
+    "username": "custMah1",
+    "password": "1111"
+}
+*/
 
 var UserDetailList []UserDetails
 
@@ -22,10 +31,14 @@ var x int = 1
 var ind *int = &x
 
 func CreateUser() {
+
 	//reader := bufio.NewReader(os.Stdin)
 	var nm, em, pnum = "", "", ""
+	var id int
 	var bal float64
 	fmt.Println("Please enter the follwoing details to set up your account")
+	fmt.Println("Please enter the Customer Id: ")
+	fmt.Scan(&id)
 	fmt.Println("Please enter the Customer Name: ")
 	fmt.Scan(&nm)
 	fmt.Println("Please enter the Customer Email:")
@@ -34,14 +47,24 @@ func CreateUser() {
 	fmt.Scan(&pnum)
 	fmt.Println("Please enter the Customer Balance")
 	fmt.Scan(&bal)
-	Cust := UserDetails{*ind, nm, em, pnum, bal}
-	*ind = *ind + 1
-	CreateCredentials(Cust)
+	Cust := UserDetails{id, nm, em, pnum, bal, ""}
+	//*ind = *ind + 2
+	lk := CreateCredentials(Cust)
+	Cust.UserName = lk.UserName
 	UserDetailList = append(UserDetailList, Cust)
+	/*err := Database.LoadUserData(Database.DbPool, Cust)
+	if err != nil {
+		fmt.Errorf("Unable to Load User data to Database ", err)
+		return
+	}*/
+	Sd = Cust
+	//Database.LoadUserData(Database.DbPool,Sd)
+	lg.Log.Info("User created successfully ✅")
 	fmt.Println("Customer_Account & Credentials Created Successfully ✅")
 }
 
-func CreateCredentials(cust UserDetails) {
+func CreateCredentials(cust UserDetails) LoginCred {
+	lg.Log.Info("Entered the create credentials function")
 	var pass string
 	var unm string = "cust"
 	unm += cust.Cust_Name[:3] + strconv.Itoa(cust.Cust_Id)
@@ -51,11 +74,14 @@ func CreateCredentials(cust UserDetails) {
 	fmt.Scan(&pass)
 	lc := LoginCred{unm, pass}
 	LoginCredList = append(LoginCredList, lc)
+	lg.Log.Info("Login credentials created successfully ✅")
+	return lc
 }
 
+/*
 func StoreVault(str string, lc LoginCred) {
 	// Create Vault client
-
+	lg.Log.Info("Entered Store Vault Section !!")
 	client, err := api.NewClient(&api.Config{Address: "http://127.0.0.1:8200"})
 	if err != nil {
 		log.Fatal(err)
@@ -63,12 +89,16 @@ func StoreVault(str string, lc LoginCred) {
 	client.SetNamespace("admin")
 
 	// Set authentication token (replace with your actual token)
-	client.SetToken(str)
+	client.SetToken("hvs.femyXdNg4m2pl3rgsy0sqYVJ")
 
 	// Store credentials
+	fmt.Println(lc)
+	lg.Log.Info("Login details is :", lc)
 	data := map[string]interface{}{
-		"username": lc.UserName,
-		"password": lc.Password,
+		"data": map[string]interface{}{ // Vault expects the data to be nested under "data"
+			"username": lc.UserName,
+			"password": lc.Password,
+		},
 	}
 
 	_, err = client.Logical().Write("secret/data/myapp", data)
@@ -84,3 +114,4 @@ func StoreVault(str string, lc LoginCred) {
 	}
 	fmt.Println("Retrieved secret:", secret.Data)
 }
+*/
